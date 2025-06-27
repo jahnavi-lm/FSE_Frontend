c344435b7f58460e05fedaa272da4165b0712b1a
Jahnavi Lakshmi Muttireddy <jahnavi.muttireddy@bounteous.com>
2025-06-27 12:45:54 +0530

    jahnavi/fte-login initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +10,7 @@ function App() {
     <h1 className="text-3xl font-bold underline text-sky-400">
       Welcome to FSE
     </h1>
+    <Header />
     </>
   )
 }
