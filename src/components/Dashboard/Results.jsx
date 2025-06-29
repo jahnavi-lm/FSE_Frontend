966f393306edbec6a3dea14ec254cc5a4b49593a
Anshu <anshu.raj@bounteous.com>
2025-06-29 18:21:15 +0530

    Revert "investor/account pg create"

--- src/components/Dashboard/Results.jsx
+++
@@ -1,8 +0,0 @@
-export default function Results() {
-    return (
-      <div className="text-center text-gray-700">
-        <h2 className="text-2xl font-bold mb-4">Results</h2>
-        <p>your Results here (Coming Soon).</p>
-      </div>
-    );
-  }
